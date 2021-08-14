4f0402edf90eb4240dd7c3f3e1ac89224075fd9a
qtrrb <torroba.q@gmail.com>
2021-08-14 14:02:32 +0200

    🚧 Added code to pull post data from ID

--- js/main.js
+++ js/main.js
@@ -8,6 +8,17 @@ request.send();
 request.onload = function() {
     const IDs = request.response;
     for (let i=0;i<10;i++) {
-        console.log(IDs[i])
+        getContentFromID(IDs[i])
+    }
+}
+
+function getContentFromID(StoryID) {
+    let StoryRequestURL = `https://hacker-news.firebaseio.com/v0/item/${StoryID}.json?print=pretty`;
+    let request_story = new XMLHttpRequest();
+    request_story.open('GET', StoryRequestURL);
+    request_story.responseType = 'json';
+    request_story.send();
+    request_story.onload = function() {
+        console.log(request_story.response)
     }
 }
